73346769f1e307bc42715da72fdc39f971b0012a
Andrew Smith <andrew.smith@miracl.com>
2018-11-05 20:26:20 +0000

    Ensure we cancel the context

--- example/main.go
+++ example/main.go
@@ -16,7 +16,8 @@ func main() {
 	svr := &http.Server{Addr: ":8080"}
 
 	irpt := interrupter.New(func() {
-		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
 		svr.Shutdown(ctx)
 	})
 	defer irpt.Close()
